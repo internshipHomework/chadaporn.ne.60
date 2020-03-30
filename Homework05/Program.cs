1b3f931bfa980f9dc989cf55caa3aa0c7802b1b5
chadaporn29797 <chadaporn.ne.60@ubu.ac.th>
2020-03-30 14:04:39 +0700

    ส่งการบ้าน

---
+++ Homework05/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace work2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array1 = new string[] { "1","2","3","4","5","6","7","8","9","A" };
+            string[] array3 = new string[] { "[ ]","[ ]","[ ]","[ ]","[ ]","[ ]","[ ]","[ ]","[ ]","[ ]" };
+            Console.WriteLine(string.Join(" ", array3));
+            Console.WriteLine(" 1   2   3   4   5   6   7   8   9   A");
+            int count = 1;
+            while ( count <= 100 ) {
+                Console.Write("Please choose LED to turn On/Off: ");
+                string ledch = Console.ReadLine();
+                for (int i = 0; i < 10; i++) {
+                    if ( array1[i] == ledch & array3[i] == "[!]" ){
+                        array3[i] = "[ ]";
+                    }
+                    else if ( array1[i] == ledch & array3[i] == "[ ]" ){
+                        array3[i] = "[!]";
+                    }
+                }
+                Console.WriteLine(string.Join(" ", array3));
+                Console.WriteLine(" 1   2   3   4   5   6   7   8   9   A");
+                
+                count++;
+            }
+
+        }
+    }
+}
