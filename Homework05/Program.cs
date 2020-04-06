57298d578adb8c4d2cd404e38f3e142d67f308dd
chadaporn29797 <chadaporn.ne.60@ubu.ac.th>
2020-04-06 15:54:52 +0700

    ส่งงาน

--- Homework05/Program.cs
+++ Homework05/Program.cs
@@ -1,33 +1,71 @@
 ﻿using System;
 
-namespace work2
+namespace work05
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            string[] array1 = new string[] { "1","2","3","4","5","6","7","8","9","A" };
-            string[] array3 = new string[] { "[ ]","[ ]","[ ]","[ ]","[ ]","[ ]","[ ]","[ ]","[ ]","[ ]" };
-            Console.WriteLine(string.Join(" ", array3));
-            Console.WriteLine(" 1   2   3   4   5   6   7   8   9   A");
-            int count = 1;
-            while ( count <= 100 ) {
-                Console.Write("Please choose LED to turn On/Off: ");
-                string ledch = Console.ReadLine();
-                for (int i = 0; i < 10; i++) {
-                    if ( array1[i] == ledch & array3[i] == "[!]" ){
-                        array3[i] = "[ ]";
-                    }
-                    else if ( array1[i] == ledch & array3[i] == "[ ]" ){
-                        array3[i] = "[!]";
-                    }
-                }
-                Console.WriteLine(string.Join(" ", array3));
-                Console.WriteLine(" 1   2   3   4   5   6   7   8   9   A");
-                
-                count++;
-            }
+  interface IHomework05
+  {
+    string DisplayLEDOnScreen(string ledNo);
+  }
 
+  class LEDSetting : IHomework05
+  {
+    string[] array1 = new string[] { "1","2","3","4","5","6","7","8","9","A" };
+    string[] array3 = new string[] { "[ ]","[ ]","[ ]","[ ]","[ ]","[ ]","[ ]","[ ]","[ ]","[ ]" };
+
+    public LEDSetting() {}
+
+    public void Fix(String ledNo) 
+    {
+        for (int i = 0; i < 10; i++) {
+            if ( array1[i] == ledNo & array3[i] == "[!]" ){
+                array3[i] = "[ ]";
+            }
+            else if ( array1[i] == ledNo & array3[i] == "[ ]" ){
+                array3[i] = "[!]";
+            }
         }
     }
+    public string Display() 
+    { 
+        string display = "";
+        for (int i = 0; i < 10; i++) {
+            if ( array3[i] == "[!]" ){
+                display += array3[i] + " ";
+            }
+            else if ( array3[i] == "[ ]" ){
+                display += array3[i] + " ";
+            }
+        }
+        return display;
+    }
+    public String DisplayLEDOnScreen(string ledNo){
+        Fix(ledNo);
+        return Display();
+    }
+    public void Output1(){
+        Console.WriteLine(string.Join(" ", array3));
+        Console.WriteLine(string.Join("   ", array1));
+
+    }
+    public void Output2(){
+        Console.Write("Please choose LED to turn On/Off: ");
+        string ledNo = Console.ReadLine();
+        Console.WriteLine(DisplayLEDOnScreen(ledNo));
+        Console.WriteLine(string.Join("   ", array1));
+
+    }
+  }
+
+  class App {
+    static void Main(string[] args)
+    {
+      LEDSetting panel = new LEDSetting();
+      int count = 1;
+      panel.Output1();
+        while ( count <= 100 ) {
+            panel.Output2();
+            count++;
+        }
+    }
+  }
 }
